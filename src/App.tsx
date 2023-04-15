d5ba9cdd25c469633a9cb8f3e9cb71c7d21f07f6
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-04-15 12:22:48 +0200

    Added chakra ui + setup socket io + pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { io } from "socket.io-client";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0)
+	useEffect(() => {
+		// Connect to the Socket.io server
+		const socket = io("http://localhost:8000"); // Replace with your Socket.io server URL
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+		// Listen for events from the server
+		socket.on("welcome", (data) => {
+			console.log("Message from server:", data);
+		});
+
+		// Clean up the Socket.io connection on component unmount
+		return () => {
+			socket.disconnect();
+		};
+	}, []);
+
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/about",
+			element: <About />,
+		},
+	]);
+
+	return (
+		<ChakraProvider>
+			<RouterProvider router={router} />
+		</ChakraProvider>
+	);
 }
 
-export default App
+export default App;
