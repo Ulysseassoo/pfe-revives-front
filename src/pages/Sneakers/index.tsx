fe727112776606312f61edf27c4e2bde386f619a
fabian222222 <fabian.zuo@supinternet.fr>
2023-04-17 04:22:55 +0200

    add components to sneakers page

--- src/pages/Sneakers/index.tsx
+++ src/pages/Sneakers/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+
+import TitleWithLogoBackground from '@components/Common/TitleWithLogoBackground'
+import ShoeListing from '@components/Sneakers/ShoeListing'
+import SecondLife from '@components/Sneakers/SecondLife'
+
+import { dummyShoes } from '@dummyDatas/Shoes'
 
 const Sneakers = () => {
 	return (
-		<div>
-			
-		</div>
+		<Box
+			paddingX={20}
+		>
+			<TitleWithLogoBackground title='NOS SNEAKERS PRÉFÉRÉES' subtitle='PRODUIT' />
+
+			<ShoeListing filter={true} shoes={dummyShoes} />
+
+			<SecondLife />
+		</Box>
 	)
 };
 
