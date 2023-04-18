87b0deadabba38c997dc6f82b65c0f8498d9f4bb
fabian222222 <fabian.zuo@supinternet.fr>
2023-04-19 01:59:18 +0200

    sneaker page

--- src/pages/Sneakers/index.tsx
+++ src/pages/Sneakers/index.tsx
@@ -4,19 +4,24 @@ import { Box } from '@chakra-ui/react'
 import TitleWithLogoBackground from '@components/Common/TitleWithLogoBackground'
 import ShoeListing from '@components/Sneakers/ShoeListing'
 import SecondLife from '@components/Sneakers/SecondLife'
+import SneakerCategory from '@components/Sneakers/SneakerCategory'
+import SneakerFooter from '@components/Sneakers/SneakerFooter'
 
 import { dummyShoes } from '@dummyDatas/Shoes'
 
 const Sneakers = () => {
 	return (
-		<Box
-			paddingX={20}
-		>
-			<TitleWithLogoBackground title='NOS SNEAKERS PRÉFÉRÉES' subtitle='PRODUIT' />
-
-			<ShoeListing filter={true} shoes={dummyShoes} />
-
+		<Box>
+			<Box
+				paddingX={20}
+			>
+				<TitleWithLogoBackground title='NOS SNEAKERS PRÉFÉRÉES' subtitle='PRODUIT' />
+				<ShoeListing isFilter={true} shoes={dummyShoes} />
+			</Box>
 			<SecondLife />
+			<SneakerCategory title='Les mieux notés' nbrOfShoe={3} />
+			<SneakerCategory title='Les bons plans' nbrOfShoe={3} />
+			<SneakerFooter />
 		</Box>
 	)
 };
