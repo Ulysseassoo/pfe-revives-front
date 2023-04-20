e12554fe0c25ef080f4be6f720f8bc8f79bcb69a
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-04-21 01:26:28 +0200

    Vite config fixed

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
 			"@assets": path.resolve(__dirname, "src/assets"),
 			"@components": path.resolve(__dirname, "src/components"),
 			"@pages": path.resolve(__dirname, "src/pages"),
+			"@theme": path.resolve(__dirname, "src/theme"),
 			"@dummyDatas": path.resolve(__dirname, "src/dummyDatas"),
 			"@interfcae": path.resolve(__dirname, "src/interface"),
 		},
