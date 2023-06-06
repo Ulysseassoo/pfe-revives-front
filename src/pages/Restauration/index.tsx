6124b2b47cfc6ad1d404c0e45fcc183b9f7cdbbc
fabian222222 <fabian.zuo@supinternet.fr>
2023-06-06 04:01:56 +0200

    fix: restauration page into compoennts

--- src/pages/Restauration/index.tsx
+++ src/pages/Restauration/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
+
+import Header from '@components/Restauration/Header'
+import Steps from '@components/Restauration/Steps';
+import BeforeAfter from '@components/Restauration/BeforeAfter';
+import SecondLife from "@components/Sneakers/SecondLife";
+import SneakerCategory from '@components/Sneakers/SneakerCategory';
+import Footer from "@components/Footer";
+import SneakerFooter from "@components/Sneakers/SneakerFooter";
+
+import SecondLifeBackground from '@assets/Restauration/seconde-life.png'
 
 const Restauration = () => {
-	return <div>Restauration</div>;
+	return(
+		<div>
+			<Header />
+			<Steps />
+			<BeforeAfter />
+			<SecondLife 
+				title='Donner une seconde vie à vos sneakers !'
+				description='Vous voulez proter vos anciennces sneakers mais elles sont trop usées. Grâce à nous vous pourrez les porter à nouveau'
+				image={SecondLifeBackground}
+			/>
+			<Box
+				paddingX={20}
+				marginTop={10}
+			>
+				<SneakerCategory title="Revente de sneakers" nbrOfShoe={6} rate="3" />
+			</Box>
+			<SneakerFooter />
+			<Footer /> 
+		</div>
+	)
 };
 
 export default Restauration;
