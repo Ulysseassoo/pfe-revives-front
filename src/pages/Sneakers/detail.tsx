fddc288711978d66856f58117a8ba953fea5ea2b
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-06-30 06:06:21 +0200

    Fix shoe listing

--- src/pages/Sneakers/detail.tsx
+++ src/pages/Sneakers/detail.tsx
@@ -1,33 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"
 
-import SneakerDetail from "@components/Sneakers/SneakerDetail";
-import SneakerCategory from "@components/Sneakers/SneakerCategory";
-import SecondLife from "@components/Sneakers/SecondLife";
+import SneakerDetail from "@components/Sneakers/SneakerDetail"
+import SneakerCategory from "@components/Sneakers/SneakerCategory"
+import SecondLife from "@components/Sneakers/SecondLife"
 
-import SneakerProductDetail from "@assets/Sneakers/sneaker-product-detail.png";
+import SneakerProductDetail from "@assets/Sneakers/sneaker-product-detail.png"
 
-import Footer from "@components/Footer";
-import Newsletter from "@components/Home/Newsletter";
-import { Box } from "@chakra-ui/react";
-import { PADDING_DESKTOP, PADDING_IPAD } from "@theme/theme";
-import { useListShoesQuery } from "@store/api/Shoes";
-import SneakerDetailSkeleton from "@components/Sneakers/SneakerDetailSkeleton";
+import Footer from "@components/Footer"
+import Newsletter from "@components/Home/Newsletter"
+import { Box } from "@chakra-ui/react"
+import { PADDING_DESKTOP, PADDING_IPAD } from "@theme/theme"
+import { useListShoesQuery } from "@store/api/Shoes"
+import SneakerDetailSkeleton from "@components/Sneakers/SneakerDetailSkeleton"
 
 const detail = () => {
-	const { id } = useParams();
+	const { id } = useParams()
 	const { data, isFetching } = useListShoesQuery({
 		brand: "Jordan",
 		take: "1",
-		model: id,
-	});
+		model: id
+	})
 
 	return (
 		<Box as="section">
 			<Box paddingX={{ lg: PADDING_DESKTOP, base: PADDING_IPAD }}>
 				{data !== undefined && !isFetching ? <SneakerDetail shoe={data.data[0]} /> : <SneakerDetailSkeleton />}
 
-				<SneakerCategory title="Vous aimerez aussi" nbrOfShoe={3} rate="4" />
-				<SneakerCategory title="Voir plus de chaussures" nbrOfShoe={3} rate="1" />
+				<SneakerCategory title="Vous aimerez aussi" nbrOfShoe={3} rate="5" price={80} />
+				<SneakerCategory title="Voir plus de chaussures" nbrOfShoe={3} rate="5" price={85} />
 			</Box>
 
 			<SecondLife
@@ -41,7 +41,7 @@ const detail = () => {
 			</Box>
 			<Footer />
 		</Box>
-	);
-};
+	)
+}
 
-export default detail;
+export default detail
